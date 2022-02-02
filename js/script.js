6e5877517e9a56864029ccc1d15b68b2083a83c3
iostream231 <deeznuts267@protonmail.com>
2022-02-02 13:06:46 +0100

    Enchanced navbar && Added Themes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function changeTheme(alr) {
+    let r = document.querySelector(":root");
+    let stl = getComputedStyle(r);
+    switch(stl.getPropertyValue("--bcolor").toString()) {
+        case "rgb(40, 40, 40)" :
+            r.style.setProperty("--bcolor" , "rgb(255, 255, 255)");
+            r.style.setProperty("--inbcolor" , "rgb(185, 185, 185)");
+            r.style.setProperty("--txtcolor", "rgb(0, 0, 0)");
+            document.querySelector(".them-dev a i").className = "fas fa-moon"
+            break;
+        case "rgb(255, 255, 255)" :
+            r.style.setProperty("--bcolor" , "rgb(40, 40, 40)");
+            r.style.setProperty("--inbcolor" , "rgb(10, 10, 10)");
+            r.style.setProperty("--txtcolor", "#fff");
+            document.querySelector(".them-dev a i").className = "fas fa-sun"
+            break;
+    }
+}
