1305e72a4abb6cf31e892486037842c511685e89
iostream231 <deeznuts267@protonmail.com>
2022-02-13 15:04:53 +0100

    Added Light Theme && Info Section Looks More Less Shit Than Before

--- js/script.main.js
+++ js/script.main.js
@@ -1,19 +1,41 @@
+const themes = {
+    light:{
+        bcolor:"#fff",
+        inbcolor:"#ddd",
+        txtcolor:"#4D46B9",
+        inttxtcolor:"#4679B9",
+        oppositecolor:"#000",
+    },
+    dark:{
+        bcolor:"#00001C",
+        inbcolor:"#00000D",
+        txtcolor:"#9796FF",
+        inttxtcolor:"#C4C4FF",
+        oppositecolor:"#fff",
+    },
+};
+
+
 function changeTheme(alr) {
     let r = document.querySelector(":root");
     let stl = getComputedStyle(r);
-    switch(stl.getPropertyValue("--bcolor").toString()) {
-        case "rgb(40, 40, 40)" :
-            r.style.setProperty("--bcolor" , "rgb(255, 255, 255)");
-            r.style.setProperty("--inbcolor" , "rgb(185, 185, 185)");
-            r.style.setProperty("--txtcolor", "rgb(0, 0, 0)");
+    switch(stl.getPropertyValue("--oppcolor").toString()) {
+        case "#fff" :
+            r.style.setProperty("--bcolor" , themes.light.bcolor);
+            r.style.setProperty("--inbcolor" , themes.light.inbcolor);
+            r.style.setProperty("--txtcolor", themes.light.txtcolor);
+            r.style.setProperty("--inttxtcolor", themes.light.inttxtcolor);
+            r.style.setProperty("--oppcolor" , themes.light.oppositecolor);
             document.querySelector(".fas.fa-sun").className = "fas fa-moon";
             document.getElementById("myCanvas").style.display = "none";
             sessionStorage.setItem("theme" , "light");
             break;
-        case "rgb(255, 255, 255)" :
-            r.style.setProperty("--bcolor" , "rgb(40, 40, 40)");
-            r.style.setProperty("--inbcolor" , "rgb(10, 10, 10)");
-            r.style.setProperty("--txtcolor", "#fff");
+        case "#000" :
+            r.style.setProperty("--bcolor" , themes.dark.bcolor);
+            r.style.setProperty("--inbcolor" , themes.dark.inbcolor);
+            r.style.setProperty("--txtcolor", themes.dark.txtcolor);
+            r.style.setProperty("--inttxtcolor", themes.dark.inttxtcolor);
+            r.style.setProperty("--oppcolor" , themes.dark.oppositecolor);
             document.querySelector(".fas.fa-moon").className = "fas fa-sun";
             sessionStorage.setItem("theme" , "dark");
             document.getElementById("myCanvas").style.display = "block";
